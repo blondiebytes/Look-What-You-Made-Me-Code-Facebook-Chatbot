d34403b505b04516895c58dab5fcdab6cf067f4c
blondiebytes <blondiebytes@gmail.com>
2018-09-14 10:42:06 -0400

    Init

---
+++ index.js
@@ -0,0 +1,71 @@
+'use strict'
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const app = express()
+
+app.set('port', (process.env.PORT || 5000))
+
+// Process application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
+
+// Process application/json
+app.use(bodyParser.json())
+
+// Index route
+app.get('/', function (req, res) {
+    res.send('Hello world! I know lots of Taylor Swift Lyrics. Ask me for one.')
+})
+
+// for Facebook verification
+app.get('/webhook/', function (req, res) {
+    if (req.query['hub.verify_token'] === 'codebot') {
+        res.send(req.query['hub.challenge'])
+    }
+    res.send('Error, wrong token')
+})
+
+app.post('/webhook/', function (req, res) {
+    let messaging_events = req.body.entry[0].messaging
+    for (let i = 0; i < messaging_events.length; i++) {
+        let event = req.body.entry[0].messaging[i]
+        let sender = event.sender.id
+        if (event.message && event.message.text) {
+            let text = event.message.text
+            decideMessage(sender);
+        }
+    }
+    res.sendStatus(200)
+})
+
+const token = "EAADk7mvTLxEBACgR50UQmZA8ERQL3EIBN9ZCD5F4wqGlx5XVjolaDZBBAHE6UCXo6rxHvt45TLmD5SupKuLHfh2oECpfmzPnIqvNEnNXH0v2qT8a6sqLVTvObSxZCv9uIdCJWPbPkkeITDnRO3axn7oCY7CEpxZBmS8ZBc4rEU8AZDZD"
+
+function decideMessage(sender) {
+    sendTextMessage("You Belong With Me!")
+}
+
+function sendTextMessage(sender, text) {
+    let messageData = { text:text }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
+
+// Spin up the server
+app.listen(app.get('port'), function() {
+    console.log('running on port', app.get('port'))
+})
+
